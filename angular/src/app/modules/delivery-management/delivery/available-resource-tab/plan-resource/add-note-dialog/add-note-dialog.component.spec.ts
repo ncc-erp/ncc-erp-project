6d92841f258692b29b66dedcffa001c8ec1a35ae
longduongvan <longduongvan@ncc.asia>
2023-12-13 16:05:21 +0700

    task #54421

--- angular/src/app/modules/delivery-management/delivery/available-resource-tab/plan-resource/add-note-dialog/add-note-dialog.component.spec.ts
+++ angular/src/app/modules/delivery-management/delivery/available-resource-tab/plan-resource/add-note-dialog/add-note-dialog.component.spec.ts
@@ -1,16 +1,15 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { AddNoteDialogComponent } from './add-note-dialog.component';
+import { AddNoteDialogComponent } from "./add-note-dialog.component";
 
-describe('AddNoteDialogComponent', () => {
+describe("AddNoteDialogComponent", () => {
   let component: AddNoteDialogComponent;
   let fixture: ComponentFixture<AddNoteDialogComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ AddNoteDialogComponent ]
-    })
-    .compileComponents();
+      declarations: [AddNoteDialogComponent],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -19,7 +18,7 @@ describe('AddNoteDialogComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
--- angular/src/app/modules/delivery-management/delivery/available-resource-tab/plan-resource/add-note-dialog/add-note-dialog.component.ts
+++ angular/src/app/modules/delivery-management/delivery/available-resource-tab/plan-resource/add-note-dialog/add-note-dialog.component.ts
@@ -1,40 +1,42 @@
-import { ResourceManagerService } from './../../../../../../service/api/resource-manager.service';
-import { DeliveryResourceRequestService } from './../../../../../../service/api/delivery-request-resource.service';
-import { UserService } from './../../../../../../service/api/user.service';
-import { IUser } from './../../../../../../service/model/user.inteface';
-
+import { ResourceManagerService } from "./../../../../../../service/api/resource-manager.service";
+import { UserService } from "./../../../../../../service/api/user.service";
 import {
   Component,
   EventEmitter,
+  Inject,
   OnDestroy,
   OnInit,
   Output,
-} from '@angular/core';
-import { BsModalRef } from 'ngx-bootstrap/modal';
-import { Subscription } from 'rxjs';
-import { finalize } from 'rxjs/operators';
+} from "@angular/core";
+import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
+import { Subscription } from "rxjs";
+import { finalize } from "rxjs/operators";
 
 @Component({
-  selector: 'app-add-note-dialog',
-  templateUrl: './add-note-dialog.component.html',
-  styleUrls: ['./add-note-dialog.component.css'],
+  selector: "app-add-note-dialog",
+  templateUrl: "./add-note-dialog.component.html",
+  styleUrls: ["./add-note-dialog.component.css"],
 })
 export class AddNoteDialogComponent implements OnInit, OnDestroy {
-  fullName: string;
-  id: number;
-  poolNote: string = '';
+  poolNote: string = "";
   userId: number;
 
   saving = false;
   @Output() onSave = new EventEmitter<null>();
 
   subscription: Subscription[] = [];
-  constructor(public bsModalRef: BsModalRef, public userService: UserService, private resourceService:ResourceManagerService) {}
+
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    public dialogRef: MatDialogRef<AddNoteDialogComponent>,
+    public userService: UserService,
+    private resourceService: ResourceManagerService
+  ) {}
 
   ngOnInit(): void {
-    if (this.id) {
+    if (this.data.id) {
       this.subscription.push(
-        this.userService.getOne(this.id).subscribe((response) => {
+        this.userService.getOne(this.data.id).subscribe((response) => {
           this.userId = response.result.id;
           this.poolNote = response.result.poolNote;
         })
@@ -44,9 +46,9 @@ export class AddNoteDialogComponent implements OnInit, OnDestroy {
 
   SaveAndClose() {
     let requestBody = {
-      userId : this.userId,
-      note: this.poolNote
-    }
+      userId: this.userId,
+      note: this.poolNote,
+    };
     this.saving = true;
     this.subscription.push(
       this.resourceService
@@ -57,9 +59,9 @@ export class AddNoteDialogComponent implements OnInit, OnDestroy {
           })
         )
         .subscribe(() => {
-          this.bsModalRef.hide();
+          this.dialogRef.close();
           this.onSave.emit();
-          abp.notify.success("Update Note")
+          abp.notify.success("Update Note");
         })
     );
   }

a88fe373bb7ccf17cae8e55a0db362cde5705568
nam.dohoang <nam.dohoang@ncc.asia>
2024-03-13 15:13:10 +0700

    #54986 (Weekly Reports > Weekly Detail: fix bug UI Note in Current resource and Bill accounts)

---
+++ aspnet-core/src/ProjectManagement.Application/APIs/ProjectUsers/UpdateNoteCurrentResourceDto.cs
@@ -0,0 +1,12 @@
+﻿using Abp.Application.Services.Dto;
+using Abp.AutoMapper;
+using ProjectManagement.Entities;
+
+namespace ProjectManagement.APIs.ProjectUsers
+{
+    [AutoMapTo(typeof(ProjectUser))]
+    public class UpdateNoteCurrentResourceDto : EntityDto<long>
+    {
+        public string Note { get; set; }
+    }
+}

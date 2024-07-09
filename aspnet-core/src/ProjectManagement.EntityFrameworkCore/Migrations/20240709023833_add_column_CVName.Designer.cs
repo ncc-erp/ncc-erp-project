d3c66cb2d14426f6123a2c436942f22669efe408
dat.dotien <dat.dotien@ncc.asia>
2024-07-09 09:40:12 +0700

    add_file_migration

--- aspnet-core/src/ProjectManagement.EntityFrameworkCore/Migrations/20240709023833_add_column_CVName.Designer.cs
+++ aspnet-core/src/ProjectManagement.EntityFrameworkCore/Migrations/20240709023833_add_column_CVName.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProjectManagement.EntityFrameworkCore;
 
 namespace ProjectManagement.Migrations
 {
     [DbContext(typeof(ProjectManagementDbContext))]
-    partial class ProjectManagementDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240709023833_add_column_CVName")]
+    partial class add_column_CVName
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

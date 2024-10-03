8ceb2b908b4e32991cc715973352416ef47ab22d
kha.letuan <kha.letuan@ncc.asia>
2024-10-03 14:45:13 +0700

    feat: implement Create API to add new CVStatus

---
+++ aspnet-core/src/ProjectManagement.Application/APIs/CvStatus/CvStatusAppService.cs
@@ -0,0 +1,32 @@
+﻿using Abp.Authorization;
+using Abp.UI;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NccCore.Extension;
+using NccCore.Paging;
+using ProjectManagement.APIs.CvStatus.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Dynamic.Core;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManagement.APIs.CvStatus
+{
+    [AbpAuthorize]
+    public class CvStatusAppService : ProjectManagementAppServiceBase
+    {
+        [HttpPost]
+        public async Task<CvStatusCreateEditDto> Create(CvStatusCreateEditDto input)
+        {
+            var checkName = await WorkScope.GetAll<Entities.CvStatus>().AnyAsync(cv => cv.Name == input.Name);
+            if (checkName)
+            {
+                throw new UserFriendlyException("Name already exists !");
+            }
+            await WorkScope.InsertAsync(ObjectMapper.Map<Entities.CvStatus>(input));
+            return input;
+        }
+    }
+}

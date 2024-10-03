b059f8c79b7ef55cd9b7ab2ff1ccb0df5d0d9c8d
kha.letuan <kha.letuan@ncc.asia>
2024-10-03 13:36:04 +0700

    feat: add table CvStatus to database

---
+++ aspnet-core/src/ProjectManagement.Core/Entities/CvStatus.cs
@@ -0,0 +1,13 @@
+﻿using Abp.Domain.Entities.Auditing;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectManagement.Entities
+{
+    public class CvStatus : FullAuditedEntity<long>
+    {
+        public string Name { get; set; }
+        public string Color { get; set; }
+    }
+}

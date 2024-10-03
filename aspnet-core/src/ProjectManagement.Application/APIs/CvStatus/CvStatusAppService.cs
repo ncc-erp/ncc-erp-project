3fba690452ee0d9f310b0817e2bb2acf0d3bc6dc
kha.letuan <kha.letuan@ncc.asia>
2024-10-03 16:51:12 +0700

    feat: implement Update and Delete API to update and delete CVStatus

--- aspnet-core/src/ProjectManagement.Application/APIs/CvStatus/CvStatusAppService.cs
+++ aspnet-core/src/ProjectManagement.Application/APIs/CvStatus/CvStatusAppService.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using NccCore.Extension;
 using NccCore.Paging;
 using ProjectManagement.APIs.CvStatus.Dto;
+using ProjectManagement.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -40,6 +41,31 @@ namespace ProjectManagement.APIs.CvStatus
                     Color = cv.Color,
                 });
             return await query.GetGridResult(query, input);
+        }
+
+        [HttpPut]
+        public async Task<CvStatusCreateEditDto> Update(long id, CvStatusCreateEditDto input)
+        {
+            var cvStatus = await WorkScope.GetAsync<Entities.CvStatus>(id);
+            var checkName = await WorkScope.GetAll<Entities.CvStatus>().AnyAsync(cv => cv.Name == input.Name && cv.Id != id);
+            if (checkName)
+            {
+                throw new UserFriendlyException("Name already exists !");
+            }
+            await WorkScope.UpdateAsync(ObjectMapper.Map<CvStatusCreateEditDto, Entities.CvStatus>(input, cvStatus));
+            return input;
+        }
+
+        [HttpDelete]
+        public async Task Delete(long id)
+        {
+            var cvStatus = await WorkScope.GetAsync<Entities.CvStatus>(id);
+            var isUsed = await WorkScope.GetAll<ResourceRequestCV>().AnyAsync(cv => cv.CvStatusId == id);
+            if (isUsed)
+            {
+                throw new UserFriendlyException($"CVStatus ID {id} is being used");
+            }
+            await WorkScope.GetRepo<Entities.CvStatus>().DeleteAsync(cvStatus);
         }
     }
 }

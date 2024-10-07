67f89bf368c42a8f9ee92f640f632d7eaa164b32
kha.letuan <kha.letuan@ncc.asia>
2024-10-07 09:32:25 +0700

    feat: fetch data cvstatus into UI

--- angular/src/app/modules/admin/cvstatus/cvstatus.component.ts
+++ angular/src/app/modules/admin/cvstatus/cvstatus.component.ts
@@ -1,38 +1,37 @@
 import { Component, Injector, OnInit } from '@angular/core';
 import { CreateUpdateCvstatusComponent } from './create-update-cvstatus/create-update-cvstatus.component';
 import { MatDialog } from '@angular/material/dialog';
+import { PagedListingComponentBase, PagedRequestDto } from '@shared/paged-listing-component-base';
+import { CvstatusService } from '../../../service/api/cvstatus.service';
+import { catchError, finalize } from 'rxjs/operators';
 
 @Component({
   selector: 'app-cvstatus',
   templateUrl: './cvstatus.component.html',
   styleUrls: ['./cvstatus.component.css']
 })
-export class CVStatusComponent implements OnInit {
+export class CVStatusComponent extends PagedListingComponentBase<CVStatusComponent> implements OnInit {
+  protected list(request: PagedRequestDto, pageNumber: number, finishedCallback: Function): void {
+    this.cvStatusService.getAllPaging(request).pipe(finalize(() => {
+      finishedCallback();
+    }), catchError(this.cvStatusService.handleError)).subscribe(data => {
+      this.cVStatusList = data.result.items;
+      this.showPaging(data.result, pageNumber)
+    })
+  }
+  protected delete(entity: CVStatusComponent): void {
+    throw new Error('Method not implemented.');
+  }
   
   constructor(private dialog: MatDialog,
     injector: Injector,
+    private cvStatusService: CvstatusService
   ) {
+    super(injector);
   }
 
-  cVStatusList = [
-    {
-      id: 1,
-      name: 'Chưa làm CV',
-      color: '#232323'
-    },
-    {
-      id: 2,
-      name: 'Đã làm CV',
-      color: '#543234'
-    },
-    {
-      id: 3,
-      name: 'Đang làm CV',
-      color: '#234234'
-    }
-  ]
-
   ngOnInit(): void {
+    this.refresh();
   }
 
   public showDialog() {

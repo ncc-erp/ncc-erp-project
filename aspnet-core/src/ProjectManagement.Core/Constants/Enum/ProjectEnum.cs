e03c8bfe05c735cff7a94c5b8149954076521c75
kha.letuan <kha.letuan@ncc.asia>
2024-10-14 13:29:10 +0700

    feat: add field WorkingType into ProjectUsers, create migration

--- aspnet-core/src/ProjectManagement.Core/Constants/Enum/ProjectEnum.cs
+++ aspnet-core/src/ProjectManagement.Core/Constants/Enum/ProjectEnum.cs
@@ -361,5 +361,12 @@
             Fail = 8,
             Onboarded = 9,
         }
+
+        public enum ProjectUserWorkingType
+        {
+            Offical = 0,
+            Support = 1,
+            Training = 2
+        }
     }
 }

0aefd6414a4de09fbc85afdac0ee83852b7d4bd5
kha.letuan <kha.letuan@ncc.asia>
2024-10-24 15:04:02 +0700

    feat: add foreign key for table ResourceRequestCV

--- aspnet-core/src/ProjectManagement.EntityFrameworkCore/Migrations/20241024065739_Add_Foreignkey_ResourceRequestCV.Designer.cs
+++ aspnet-core/src/ProjectManagement.EntityFrameworkCore/Migrations/20241024065739_Add_Foreignkey_ResourceRequestCV.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProjectManagement.EntityFrameworkCore;
 
 namespace ProjectManagement.Migrations
 {
     [DbContext(typeof(ProjectManagementDbContext))]
-    partial class ProjectManagementDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241024065739_Add_Foreignkey_ResourceRequestCV")]
+    partial class Add_Foreignkey_ResourceRequestCV
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

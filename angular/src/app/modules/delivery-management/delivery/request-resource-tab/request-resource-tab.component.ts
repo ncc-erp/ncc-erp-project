83d0e6a69dc9ecd5ae0fbb563d1782cbecf8dace
khaletuanncc <kha.letuan@ncc.asia>
2024-10-29 10:44:07 +0700

    feat: resource request improvement (#277)

--- angular/src/app/modules/delivery-management/delivery/request-resource-tab/request-resource-tab.component.ts
+++ angular/src/app/modules/delivery-management/delivery/request-resource-tab/request-resource-tab.component.ts
@@ -141,6 +141,24 @@ export class RequestResourceTabComponent
 
   public isExpand: boolean = false;
 
+  public codeColors = [
+    '#343434',
+    '#055E68',
+    '#62A388',
+    '#B9D2D2',
+    '#430D27',
+    '#C94E4E',
+    '#0E1555',
+    '#4B2B30',
+    '#241E92',
+    '#FF9898'
+  ];
+  public codeColorMap: { [key: string]: string } = {};
+  public trackColor: number = 0;
+
+  public hoveredRow: { [key: number]: { [key: number]: { [key: string]: boolean } } } = {}
+
+
   ResourceRequest_View = PERMISSIONS_CONSTANT.ResourceRequest_View;
   ResourceRequest_PlanNewResourceForRequest = PERMISSIONS_CONSTANT.ResourceRequest_PlanNewResourceForRequest;
   ResourceRequest_UpdateResourceRequestPlan = PERMISSIONS_CONSTANT.ResourceRequest_UpdateResourceRequestPlan;
@@ -205,12 +223,30 @@ export class RequestResourceTabComponent
     }
   }
 
-  edit(index: number, field: string, item: ResourceRequestCVDto, requestId: number): void {
+  edit(index: number, field: string, item: ResourceRequestCVDto, requestId: number, resCv: ResourceRequestCVDto[]): void {
+    if (this.originalValues !== undefined) {
+      const originalRequestId = Object.keys(this.originalValues).map(Number);
+      if(originalRequestId.length > 0)
+      {
+        const originalIndex = Object.keys(this.originalValues[originalRequestId[0]]);
+        const originalField = Object.keys(this.originalValues[originalRequestId[0]][originalIndex[0]]);
+        if(requestId === originalRequestId[0]) {
+          resCv[originalIndex[0]][originalField] = this.originalValues[requestId][originalIndex[0]][originalField[0]];
+        }
+        else{
+          const po = this.listRequest.findIndex(res => res.id === originalRequestId[0]);
+          if (po !== -1) {
+            this.listRequest[po].resCV[originalIndex[0]][originalField[0]]  = this.originalValues[originalRequestId[0]][originalIndex[0]][originalField[0]];
+          }
+        }
+      }
+    } 
     this.editingRows = {};
     this.originalValues = {};
     this.originalValues[requestId] = { [index]: { [field]: item[field] } };
     this.editingRows[requestId] = { [index]: { [field]: true } };
   }
+
   updateStatusCV(index: number, item: ResourceRequestCVDto, field: string, requestId: number) {
     const res = {
       resourceRequestCVId: item.id,
@@ -276,7 +312,7 @@ export class RequestResourceTabComponent
           delete this.editingRows[requestId][index][field];
         }
         if (this.originalValues[requestId][index]) {
-          this.originalValues[requestId][index][field] = res.sendCVDate;
+          this.originalValues[requestId][index][field] = result.result.sendCVDate;
         }
       },
       error => {
@@ -297,7 +333,7 @@ export class RequestResourceTabComponent
           delete this.editingRows[requestId][index][field];
         }
         if (this.originalValues[requestId][index]) {
-          this.originalValues[requestId][index][field] = res.interviewDate;
+          this.originalValues[requestId][index][field] = result.result.interviewDate;
         }
       },
       error => {
@@ -508,9 +544,10 @@ export class RequestResourceTabComponent
           this.resourceRequestService.UpdateBillInfoPlan(req)
             .pipe(catchError(this.resourceRequestService.handleError))
             .subscribe({
-              next: (data: { result: { billUserInfo: any; cvName: string } }) => {
+              next: (data: { result: { billUserInfo: any; cvName: string; planUserInfo: any } }) => {
                 if(data?.result) {
                   request.billUserInfo = data.result.billUserInfo;
+                  request.planUserInfo = data.result.planUserInfo;
                   request.cvName = data.result.cvName;
                 }
               },
@@ -785,9 +822,11 @@ export class RequestResourceTabComponent
       maxHeight: "90vh",
     });
     show.afterClosed().subscribe((rs) => {
-      item.billUserInfo = rs.data.billUserInfo;
-      item.planUserInfo = rs.data.planUserInfo;
-      item.cvName = rs.data.cvName;
+      if(rs) {
+        item.billUserInfo = rs.data.billUserInfo;
+        item.planUserInfo = rs.data.planUserInfo;
+        item.cvName = rs.data.cvName;
+      }
     });
   }
 
@@ -820,6 +859,7 @@ export class RequestResourceTabComponent
           this.resourceRequestService.UpdateBillInfoPlan(input).pipe(catchError(this.resourceRequestService.handleError)).subscribe(data => {
             abp.notify.success(` Bill Account Removed Successfully!`);
             item.billUserInfo = null;
+            item.cvName = "";
           }, () => {
           })
         }
@@ -1262,6 +1302,9 @@ export class RequestResourceTabComponent
   }
 
   refreshDate(index: number, item: ResourceRequestCVDto, field: string, requestId: number) {
+    if (this.originalValues[requestId] && this.originalValues[requestId][index]) {
+      this.originalValues[requestId][index][field] = null;
+    }
     if(field == 'sendCVDate') {
       item.sendCVDate = null;
       this.updateSendCVDate(index, item, field, requestId);
@@ -1290,6 +1333,30 @@ export class RequestResourceTabComponent
       });
     }
   }
+
+  getColorForCode(code: string): string {
+    if (!this.codeColorMap[code]) {
+      this.codeColorMap[code] = this.codeColors[this.trackColor];
+      this.trackColor = (this.trackColor + 1) % this.codeColors.length;
+    }
+    return this.codeColorMap[code];
+  }
+
+  onMouseOverInput(index: number, field: string, requestId: number): void {
+    if (!this.hoveredRow[requestId]) {
+      this.hoveredRow[requestId] = {};
+    }
+    if (!this.hoveredRow[requestId][index]) {
+      this.hoveredRow[requestId][index] = {};
+    }
+    this.hoveredRow[requestId][index][field] = true;
+  }
+
+  onMouseLeaveInput(index: number, field: string, requestId: number): void {
+    if (this.hoveredRow[requestId] && this.hoveredRow[requestId][index]) {
+      delete this.hoveredRow[requestId][index][field];
+    }
+  } 
 }
 
 export class THeadTable {

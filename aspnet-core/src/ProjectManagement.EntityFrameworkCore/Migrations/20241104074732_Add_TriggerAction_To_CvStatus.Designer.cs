95d9f8ae912d8dcec617e93082ef8c503f92b304
kha.letuan <kha.letuan@ncc.asia>
2024-11-04 14:52:09 +0700

    feat: add trigger action to cvstatus table

--- aspnet-core/src/ProjectManagement.EntityFrameworkCore/Migrations/20241104074732_Add_TriggerAction_To_CvStatus.Designer.cs
+++ aspnet-core/src/ProjectManagement.EntityFrameworkCore/Migrations/20241104074732_Add_TriggerAction_To_CvStatus.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProjectManagement.EntityFrameworkCore;
 
 namespace ProjectManagement.Migrations
 {
     [DbContext(typeof(ProjectManagementDbContext))]
-    partial class ProjectManagementDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241104074732_Add_TriggerAction_To_CvStatus")]
+    partial class Add_TriggerAction_To_CvStatus
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

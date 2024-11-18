0f2262e002e54fafcd1dbe2ad3cd2cd65a494bd3
kha.letuan <kha.letuan@ncc.asia>
2024-11-18 10:46:58 +0700

    feat(ui): update logic clear rating for user skill

--- angular/src/app/users/update-user-skill-dialog/update-user-skill-dialog.component.ts
+++ angular/src/app/users/update-user-skill-dialog/update-user-skill-dialog.component.ts
@@ -28,7 +28,7 @@ export class UpdateUserSkillDialogComponent implements OnInit {
   starCount:number = 5;
   viewStarSkillUser = false;
   public snackBarDuration: number = 2000;
-  public ratingArr = [5, 4.5, 4, 3.5, 3, 2.5, 2, 1.5, 1, 0.5, 0];
+  public ratingArr = [5, 4.5, 4, 3.5, 3, 2.5, 2, 1.5, 1, 0.5];
   public searchSkill: string = "";
   private typeUpdate: string = AppConsts.UpdateUserSkillType.DEFAULT;
   constructor(@Inject(MAT_DIALOG_DATA) public data: any,
@@ -132,12 +132,17 @@ export class UpdateUserSkillDialogComponent implements OnInit {
     }
   }
 
-  onChangeRating(rating:number, item: any) {
-    this.snackBar.open('You rated ' + rating + ' / ' + this.starCount, '', {
-      duration: this.snackBarDuration
-    });
-    item.skillRank=rating
-    return false;
+  onChangeRating(rating: number, item: any) {
+    if ((item.skillRank === 0.5 && rating === 0.5) || (item.skillRank === 1 && rating === 1)) {
+      item.skillRank = 0;
+      this.snackBar.open('Rating cleared!', '', { duration: this.snackBarDuration });
+      return;
+    }
+    if (item.skillRank === rating) {
+      return;
+    }
+    item.skillRank = rating;
+    this.snackBar.open('You rated ' + rating + ' / 5', '', { duration: this.snackBarDuration });
   }
 
   selectAll(){

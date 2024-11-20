b88568c1b1d6496b482c9799fcaa4b0b84b81494
kha.letuan <kha.letuan@ncc.asia>
2024-11-20 10:11:22 +0700

    feat: add property NameCv for ProjectUserBill

---
+++ aspnet-core/src/ProjectManagement.EntityFrameworkCore/Migrations/20241120030826_Add_NameCv_To_ProjectUserBill.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProjectManagement.Migrations
+{
+    public partial class Add_NameCv_To_ProjectUserBill : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "NameCv",
+                table: "ProjectUserBills",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NameCv",
+                table: "ProjectUserBills");
+        }
+    }
+}

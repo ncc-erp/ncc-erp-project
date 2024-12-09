ca23f7be9f359c45a29e24148eea60d9212d6c3b
kha.letuan <kha.letuan@ncc.asia>
2024-12-09 11:21:01 +0700

    feat: add new HeadCount and ConfidenceLevel properties into ResourceRequest entity

---
+++ aspnet-core/src/ProjectManagement.EntityFrameworkCore/Migrations/20241209041411_resource-request-add-headcount-confidencelevel.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProjectManagement.Migrations
+{
+    public partial class resourcerequestaddheadcountconfidencelevel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<float>(
+                name: "ConfidenceLevel",
+                table: "ResourceRequests",
+                nullable: false,
+                defaultValue: 0f);
+
+            migrationBuilder.AddColumn<float>(
+                name: "HeadCount",
+                table: "ResourceRequests",
+                nullable: false,
+                defaultValue: 0f);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ConfidenceLevel",
+                table: "ResourceRequests");
+
+            migrationBuilder.DropColumn(
+                name: "HeadCount",
+                table: "ResourceRequests");
+        }
+    }
+}

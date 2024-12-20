42e8b4c69ce896de1f0ec455a9ce4e5c3006a6c6
kha.letuan <kha.letuan@ncc.asia>
2024-12-20 10:00:18 +0700

    feat: add new table MonthlyUserContribution

---
+++ aspnet-core/src/ProjectManagement.Core/Entities/MonthlyUserContribution.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using Abp.Domain.Entities.Auditing;
+using ProjectManagement.Authorization.Users;
+using static ProjectManagement.Constants.Enum.ProjectEnum;
+
+namespace ProjectManagement.Entities
+{
+    public class MonthlyUserContribution : FullAuditedEntity<long>
+    {
+        public long UserId { get; set; }
+        [ForeignKey(nameof(UserId))]
+        public User User { get; set; }
+        
+        public long? BranchId { get; set; }
+        [ForeignKey(nameof(BranchId))]
+        public Branch Branch { get; set; }
+        
+        public long ProjectId { get; set; }
+        [ForeignKey(nameof(ProjectId))]
+        public Project Project { get; set; }
+        
+        public byte Contribute { get; set; }
+        public UserLevel UserLevel { get; set; }
+        public UserType UserType { get; set; }
+        public byte MonthTime { get; set; }
+        public int YearTime { get; set; }
+    }
+}
---
+++ aspnet-core/src/ProjectManagement.EntityFrameworkCore/Migrations/20241220025836_Add_Table_MonthlyUserContribution.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ProjectManagement.Migrations
+{
+    public partial class Add_Table_MonthlyUserContribution : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "MonthlyUserContributions",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CreationTime = table.Column<DateTime>(nullable: false),
+                    CreatorUserId = table.Column<long>(nullable: true),
+                    LastModificationTime = table.Column<DateTime>(nullable: true),
+                    LastModifierUserId = table.Column<long>(nullable: true),
+                    IsDeleted = table.Column<bool>(nullable: false),
+                    DeleterUserId = table.Column<long>(nullable: true),
+                    DeletionTime = table.Column<DateTime>(nullable: true),
+                    UserId = table.Column<long>(nullable: false),
+                    BranchId = table.Column<long>(nullable: true),
+                    ProjectId = table.Column<long>(nullable: false),
+                    Contribute = table.Column<byte>(nullable: false),
+                    UserLevel = table.Column<byte>(nullable: false),
+                    UserType = table.Column<int>(nullable: false),
+                    MonthTime = table.Column<byte>(nullable: false),
+                    YearTime = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MonthlyUserContributions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MonthlyUserContributions_Branchs_BranchId",
+                        column: x => x.BranchId,
+                        principalTable: "Branchs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_MonthlyUserContributions_Projects_ProjectId",
+                        column: x => x.ProjectId,
+                        principalTable: "Projects",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_MonthlyUserContributions_AbpUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AbpUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MonthlyUserContributions_BranchId",
+                table: "MonthlyUserContributions",
+                column: "BranchId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MonthlyUserContributions_ProjectId",
+                table: "MonthlyUserContributions",
+                column: "ProjectId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MonthlyUserContributions_UserId",
+                table: "MonthlyUserContributions",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MonthlyUserContributions");
+        }
+    }
+}
